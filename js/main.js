bb96b883cacf0558cd4205cad265132817000312
tbass44 <ts0815@live.jp>
2026-03-30 16:56:17 +0900

    PC、レスポンシブ対応クリア

--- js/main.js
+++ js/main.js
@@ -136,3 +136,25 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   }
 });
+
+
+// =========================
+// スムーススクロール
+// =========================
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    const targetId = this.getAttribute('href');
+
+    if (targetId === '#') return;
+
+    const target = document.querySelector(targetId);
+    if (!target) return;
+
+    e.preventDefault();
+
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
